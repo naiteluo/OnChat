8ed1c543b6c0857bd6421fd1aab3366cd3ebc32e
naiteluo <naiteluo@gmail.com>
2012-05-16 21:57:39 +0800

    basic pages, and 404 error page

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
 });
 
 app.configure('development', function(){
@@ -30,6 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('*', routes.error);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,14 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'OnChat' });
+};
+
+/*
+ * GET 404 error page.
+ */
+
+exports.error = function(req, res){
+	console.log("404");
+  res.render('404', { title: '404' });
 };
