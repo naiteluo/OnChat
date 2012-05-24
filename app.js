a6723ebd0d79919ee902ad423750aca57ca9d72f
naiteluo <naiteluo@gmail.com>
2012-05-25 05:44:55 +0800

    change PORT

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('*', routes.error);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
